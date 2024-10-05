cc511e139d72fa59a942f41677e40893d9e34c39
aanchalj08 <120584888+aanchalj08@users.noreply.github.com>
2024-10-05 12:41:47 +0530

    Update AddPublication.jsx

--- client/src/pages/AddPublication.jsx
+++ client/src/pages/AddPublication.jsx
@@ -19,7 +19,7 @@ const AddPublication = () => {
   const [description, setDescription] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
-  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const baseUrl = "https://lnmiit-research-portal.onrender.com";
 
   const handleAddAuthor = () => {
     setAuthors([...authors, ""]);
